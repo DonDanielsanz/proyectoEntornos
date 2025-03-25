58021e69b8d2dfec9c212bfb956a212d9e575fc1
Dani <Dani@DANIEL_PC>
2025-03-25 12:40:15 +0100

    version nuevo desarrollo

--- ProyectoEntornos/src/Paquete/Main.java
+++ ProyectoEntornos/src/Paquete/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("fichajes del betis");
+		Jugador j1=new Jugador("messi");
+		System.out.println(j1.getNombre());
 	}
 
 }
